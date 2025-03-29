709f5e1dce03bcf8a6c591192ebdb17dd970363e
Yash Yagnik <yashyagnik20@gmail.com>
2025-03-29 15:41:09 -0400

    Edits

--- PhishingEmailFlagger/phishing_detector.py
+++ PhishingEmailFlagger/phishing_detector.py
@@ -6,19 +6,22 @@ client = openai.OpenAI(api_key="")
 
 # First write what messages body or subject names would be considered to be suspicious
 
-
+sender_email_address = ""
+body = ""
 def detect_phishing():
     
     prompt = ("I have an email below and I need you to identify if it is a phising email or not. If it is a phising email then I need you to tell me the words and phrasing that allow you to identify the email is phising. Both the email address of the sender and the body of the email is below." + sender_email_address + body)
     response = client.completions.create (
     model="gpt-3.5-turbo",
-    messages=[{"role": "system", "content": prompt}]
+    prompt=prompt
+    # messages=[{"role": "system", "content": prompt}]
     ) 
+    print(response.choices[0].text.strip())
 
 def fetch_phising():
     pass 
 
-
+detect_phishing()
 # return as an array of the flagged data  
 # print(response.choices[0].text.strip())
 
