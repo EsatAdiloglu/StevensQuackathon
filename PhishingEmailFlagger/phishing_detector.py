3dd761949e3d3145577eab40009fabeb411c9944
Ryan Eshan <ryaneshan04@gmail.com>
2025-03-29 14:51:43 -0400

    Added some files and branches

---
+++ PhishingEmailFlagger/phishing_detector.py
@@ -0,0 +1,16 @@
+# Imported Packages
+
+
+
+# First write what messages body or subject names would be considered to be suspicious
+
+
+def detect_phishing():
+    #
+    pass 
+
+def fetch_phising():
+    pass 
+
+
+# return as an array of the flagged data  
