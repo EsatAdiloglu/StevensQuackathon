f6f296bfcc149b1c46a20e2d2e9e62b276aa6efb
Ryan Eshan <ryaneshan04@gmail.com>
2025-03-29 16:25:50 -0400

    gemini work

--- PhishingEmailFlagger/phishing_detector.py
+++ PhishingEmailFlagger/phishing_detector.py
@@ -1,11 +1,21 @@
 # Imported Packages
+from google import genai
+client = genai.Client(api_key="")
+
 # First write what messages body or subject names would be considered to be suspicious
-
-
 def detect_phishing():
-    #
-    pass 
+    sender_email_address = ""
+    body = ""
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents="I have an email below and I need you to identify if it is a phising email or not. If it is a phising email then I need you to tell me the words and phrasing that allow you to identify the email is phising. Both the email address of the sender and the body of the email is below."
+    )
+    
+    print(response.text)
+   
 
+#detect_phishing() 
+ 
 def fetch_phising():
     pass 
 
